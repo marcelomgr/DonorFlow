22c8b198e7ea4a6185ca8bbec58b8a5107fc6984
Marcelo Gomes Rodrigues <rodrigues.marcelog@gmail.com>
2024-06-15 10:36:03 -0300

    Criação da controller e action para obter endereço por CEP.

---
+++ DonorFlow.API/Controllers/CepController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DonorFlow.Core.Integrations.ApiCepIntegration;
+
+namespace DonorFlow.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+        private readonly IApiCepService _apiCepService;
+        public CepController(IApiCepService apiCepService)
+        {
+            _apiCepService = apiCepService;
+        }
+
+        [HttpGet("{cep}")]
+        public async Task<IActionResult> GetByCep(string cep)
+        {
+            var result = await _apiCepService.GetByCep(cep.Trim());
+            
+            return result is not null ? Ok(result) : NotFound("Cep não encontrado.");
+        }
+    }
+}
